5bc35e230be156bd58232beebe58755576e2dcdd
mr-ekansh <67186634+mr-ekansh@users.noreply.github.com>
2026-04-14 11:47:04 +0530

    B2B error changes

--- Assets/Scripts/APIs/SocketIOManager.cs
+++ Assets/Scripts/APIs/SocketIOManager.cs
@@ -212,10 +212,21 @@ public class SocketIOManager : MonoBehaviour
 
     private void OnError(Error err)
     {
-        Debug.LogError("Socket Error Message: " + err);
-#if UNITY_WEBGL && !UNITY_EDITOR
-    JSManager.SendCustomMessage("error");
-#endif
+        Debug.LogError("[ERROR] Socket error: " + err);
+        if (!string.IsNullOrEmpty(err.message) && err.message.Contains("Session expired"))
+        {
+        Debug.LogWarning("Session expired detected");
+        OnDisconnected();
+    #if UNITY_WEBGL && !UNITY_EDITOR
+        JSManager.SendCustomMessage("session_expired");
+    #endif
+        }
+        else
+        {
+    #if UNITY_WEBGL && !UNITY_EDITOR
+        JSManager.SendCustomMessage("error");
+    #endif
+        }
     }
     private void OnDisconnected() //Back2 Start
     {
@@ -824,7 +835,7 @@ public class FreeSpins
     public bool isFreeSpin { get; set; }
 }
 
-[SerializeField]
+[Serializable]
 public class Bonus
 {
     public bool istriggered { get; set; }
